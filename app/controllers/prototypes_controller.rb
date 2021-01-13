2e417fa37e6a54de3eec957a563e5d76659f82f1
Trynet123 <su508102@yahoo.co.jp>
2021-01-13 15:31:53 +0900

    投稿者以外のユーザーが、投稿者専用のページに遷移できない

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: :edit
 
   def index
     @prototypes = Prototype.all
@@ -46,5 +47,11 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless current_user.id == params[:id]
+      redirect_to root_path
+    end
   end
 end
